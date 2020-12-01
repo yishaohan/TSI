706111ac6b3255c667d95e2b5e4922364d027d81
YCZ <YICUNZHI@icloud.com>
2020-12-01 10:28:52 -0800

    auto send mail

--- src/test/java/com/ysh/talentshowintro/TalentshowintroTests.java
+++ src/test/java/com/ysh/talentshowintro/TalentshowintroTests.java
@@ -1,13 +1,30 @@
 package com.ysh.talentshowintro;
 
+import com.ysh.talentshowintro.service.MailService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @SpringBootTest
 class TalentshowintroTests {
 
-    @Test
+    @Autowired
+    MailService mailService;
+
+    @Autowired
+    TemplateEngine templateEngine;
+
     void contextLoads() {
     }
 
+    @Test
+    void mailSend() {
+//        mailService.sendSimpleMail("yicunzhi@gmail.com", "yicunzhi@icloud.com", "yicunzhi@msn.com", "测试邮件主题", "测试邮件内容");
+        Context ctx = new Context();
+        ctx.setVariable("username", "小胖胖");
+        String mail = templateEngine.process("mail.html", ctx);
+        mailService.sendSimpleMail("yicunzhi@gmail.com", "yicunzhi@icloud.com", "yicunzhi@msn.com", "测试邮件主题", mail);
+    }
 }
